8c43d7d8fd57715b08409f37b85d7fe45b793b56
Yuto Ogi <jacoyutorius@gmail.com>
2015-08-01 12:57:07 +0900

    ちょこちょこ修正。DoorkeeperAPIで次回イベントを取得するようにした

--- js/hmrb.js
+++ js/hmrb.js
@@ -1,34 +1,36 @@
-// doorkeeper API Ajax用オブジェクト
-doorkeeperApi = {
-  type: "GET",
-  url: 'http://api.doorkeeper.jp/groups/jawsug-hamamatsu/events',
-  dataType: "jsonp",
-  cache: false,
-  timeout: 5000,
-  crossDomain: true
+Date.prototype.toLocaleString = function(){
+    return [
+        this.getFullYear(),
+        this.getMonth() + 1,
+        this.getDate()
+      ].join( '.' ) + '  '
+        + this.toTimeString().slice(0, 5)
+      }
+
+var getEvents = function(data){
+  // console.table(data[0]);
+  for(var i = 0; i < data.length; i++){
+    createEventPanel(data[i]["event"]["title"],
+      new Date(data[i]["event"]["starts_at"]).toLocaleString() + "~",
+      data[i]["event"]["venue_name"],
+      data[i]["event"]["public_url"]);
+  }
 }
 
-// アクセス失敗時にその旨を表示する
-function failDoorkeeper() {
-  $('#doorkeeper-list')
-    .html(
-      $('<div class="alert alert-warning">Doorkeeper API の接続に失敗しました。</div>')
-      .fadeIn('slow')
-    );
-}
+var script = document.createElement("script");
+script.src = "https://api.doorkeeper.jp/groups/hmrb/events?callback=getEvents";
+document.head.appendChild(script);
 
-// アクセス成功時にリストを作成する
-function doneDoorkeeper(data) {
-  var arr, items = [], list, table;
 
-  $.each(data, function(){
-    items.push(parseLine(this.event));
-  });
+function createEventPanel(title, start_at, place, url){
+  var panel = $("<div></div>",{ addClass: "panel panel-default"});
+  panel.append('<div class="panel-heading panel-title"><h3>' + title + '</h3></div>');
 
-  list = '<tr>' + items.join('</tr><tr>') + '</tr>';
-  table = '<table class="table table-striped table-hover">' +
-          list +
-          '</table>';
+  var body = $('<div></div>', { addClass: "panel-body"});
+  body.append('<div class="start_at"><h3>' + start_at + '</h3></div>');
+  body.append('<div class="place"><h4>' + place + '</h4><iframe width="100%" height="80%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3280.007330403189!2d137.7245966!3d34.704995049999994!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601ade7d56c9c475%3A0x187811541505988f!2z44CSNDMyLTgwMjQg6Z2Z5bKh55yM5rWc5p2-5biC5Lit5Yy66bSo5rGf55S677yRIOa1nOadvuW4gum0qOaxn-OCouODvOODiOOCu-ODs-OCv-ODvA!5e0!3m2!1sja!2sjp!4v1431703004119" width="100%" height="450" frameborder="0" style="border:0"></iframe></div>');
+  body.append('<div class="eventUrl"><a href="' + url + '" class="btn btn-dark btn-lg">参加登録</a></div>');
+  panel.append(body);
 
-  $('#doorkeeper-list').html($(table).fadeIn('fast'));
+  $(".eventDetail").append(panel);
 }
